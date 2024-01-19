b81ede46a73011399b31fb91605ef068457bec90
mahisurrahman <mahisur.rahman.001@gmail.com>
2024-01-19 06:26:28 +0600

    Shifting to Laptop

--- src/Components/FeaturedContests/FeaturedContests.jsx
+++ src/Components/FeaturedContests/FeaturedContests.jsx
@@ -4,8 +4,8 @@ import TitleSubTitle from "../TitleSubtitle/TitleSubTitle";
 
 const FeaturedContests = () => {
     return (
-        <div className="mt-4 px-1 py-1 border-2 rounded-lg mx-auto">
-            <div className="mt-20 mb-10">
+        <div className="min-h-screen mt-4 px-1 py-1 border-2 rounded-lg mx-auto">
+            <div className="mt-10 mb-10">
                 <TitleSubTitle></TitleSubTitle>
             </div>
             <div className="grid grid-cols-5 gap-4 mb-10">
@@ -15,8 +15,8 @@ const FeaturedContests = () => {
                 <FeaturedContestCard></FeaturedContestCard>
                 <FeaturedContestCard></FeaturedContestCard>
             </div>
-            <div>
-                <Link to="/all-contests"><button>Show Call Contests</button></Link>
+            <div className="text-center mb-5">
+                <Link to="/all-contests"><button className="px-6 py-2 font-font-poppins font-bold rounded-lg bg-black text-white hover:border-2 hover:border-black hover:text-black hover:bg-white hover:cursor-pointer hover:duration-700 ">Show Call Contests</button></Link>
             </div>
         </div>
     );
