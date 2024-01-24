f417e1f200a0d2e2ac3a2ce1d2fba66685eb5223
mahisurrahman <mahisur.rahman.001@gmail.com>
2024-01-25 04:59:02 +0600

    Dynamically Navigated the Categories URL using Query String

--- src/Pages/AllContest/AllContests.jsx
+++ src/Pages/AllContest/AllContests.jsx
@@ -2,41 +2,45 @@ import { Helmet } from "react-helmet-async";
 import ContestsCard from "./ContestsCard";
 import { Fade } from "react-awesome-reveal";
 import CategoriesTitles from "./CategoriesTitles";
+import { useSearchParams } from "react-router-dom";
 
 const AllContests = () => {
   const categoriesHeading = [
     {
-      title: 'All Contests',
+      title: "All-Contests",
     },
     {
-      title: 'Website Designs',
+      title: "Web-Designs",
     },
     {
-      title: 'Coding Challenges',
+      title: "Coding-Challenges",
     },
     {
-      title: 'JS Challenges',
+      title: "JS-Challenges",
     },
     {
-      title: 'WordPress',
+      title: "WordPress",
     },
     {
-      title: 'Full Stacker',
+      title: "Full-Stacker",
     },
     {
-      title: 'Youtube Thumbnailers',
+      title: "Youtube-Thumbnailers",
     },
     {
-      title: 'Facebook Covers',
+      title: "Facebook-Covers",
     },
     {
-      title: 'Bug Solvers',
+      title: "Bug-Solvers",
     },
-  ]
+  ];
+
+  const [params, setParams] = useSearchParams();
+  const category = params.get("category");
 
   return (
     <Fade cascade damping={0.1}>
-      <div className="px-4 py-4 border-2 rounded-lg mt-2">
+      <div className="px-2 md:px-4 py-4 border-2 rounded-lg mt-2">
         <Helmet>
           <title>Contest Hub | Contests</title>
         </Helmet>
@@ -49,10 +53,14 @@ const AllContests = () => {
           </p>
         </div>
         <div className="mt-4 border-2 rounded-lg px-4 py-4">
-          <div className="flex gap-4 justify-between items-center text-sm font-font-poppins font-normal">
-            {
-              categoriesHeading.map(headings => <CategoriesTitles key={headings.title} title={headings.title}></CategoriesTitles>)
-            }
+          <div className="text-gray-600 flex gap-4 justify-between items-center text-xs md:text-sm font-font-poppins font-normal overflow-x-auto">
+            {categoriesHeading.map((headings) => (
+              <CategoriesTitles
+                key={headings.title}
+                title={headings.title}
+                selected={category === headings.title}
+              ></CategoriesTitles>
+            ))}
           </div>
         </div>
         <div className="mt-4 md:mt-10 rounded-lg py-4 grid grid-cols-1 md:grid-cols-3 gap-2 md:gap-10">
--- src/Pages/AllContest/CategoriesTitles.jsx
+++ src/Pages/AllContest/CategoriesTitles.jsx
@@ -1,9 +1,26 @@
+import qs from 'query-string';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
+const CategoriesTitles = ({title, selected}) => {
+    const [params, setParams] = useSearchParams();
+    const navigate = useNavigate();
+    const handleClick = ()=>{
+        let currentQuery = {}
+        if(params){
+            currentQuery = qs.parse(params.toString());
+            const updatedQuery = {...currentQuery, category: title}
+            const url = qs.stringifyUrl({
+                url: '/all-contests/',
+                query: updatedQuery,
+            })
 
-const CategoriesTitles = ({title}) => {
+            navigate(url);
+        }
+    }
+
     return (
-        <div>
-            <h1>{title}</h1>
+        <div  onClick={handleClick} className={`${selected?'border-b-2 border-black text-black font-bold':'border-b-none'}`}>
+            <label>{title}</label>
         </div>
     );
 };
